6e7a337f3f5d42bfc520ac4e20a637b213e55806
Leon <silenceace@gmail.com>
2021-10-26 14:28:47 +0800

    feat: store

--- src/pages/index/views/Home.vue
+++ src/pages/index/views/Home.vue
@@ -4,7 +4,7 @@
          src="@/assets/logo.png" />
     {{hello}}
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    {{apiResponseString}}
+    {{apiResponseString}}_{{currentStatus}}
   </div>
 </template>
 
@@ -13,6 +13,7 @@ import Component, { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import AxiosRequest from '@/utils/request';
 import { AxiosResponse } from 'axios';
+import store from '@/store';
 
 const api: AxiosRequest = new AxiosRequest({
   baseURL: 'https://reqbin.com/echo/post/json'
@@ -24,6 +25,7 @@ const api: AxiosRequest = new AxiosRequest({
 })
 export default class Home extends Vue {
   apiResponseString: string = '';
+  currentStatus: string = store.getters.status;
 
   apiDemo() {
     api.get('').then((res: AxiosResponse<any, any>) => {
