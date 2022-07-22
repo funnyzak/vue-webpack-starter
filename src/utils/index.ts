9dfcd628e53f4ac3a16fc82f54b318d145edc0cd
leon <silenceace@gmail.com>
2022-07-22 18:29:54 +0800

    refactor utils code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export function rangeArray(size: number) {
-  let _array: Array<number> = [];
+  const _array: Array<number> = [];
   for (let i = 0; i < size; i++) {
     _array[i] = i;
   }
@@ -10,10 +10,9 @@ export const queryString = (params?: Record<string, string>): string => {
   if (!params || params === null) {
     return '';
   }
-  return (
-    '?' +
+  return '?'.concat(
     Object.keys(params)
-      .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+      .map((k) => encodeURIComponent(k).concat('=').concat(encodeURIComponent(params[k])))
       .join('&')
   );
 };
@@ -23,21 +22,20 @@ export const queryString = (params?: Record<string, string>): string => {
  * @param url 链接
  */
 export const parse2Https = (url: string) => {
-  if (!url) return;
+  if (!url) return undefined;
   return url.startsWith('https') ? url : url.replace('http', 'https');
 };
 
 export const parseDate = function (date: string) {
-  var t = Date.parse(date);
+  const t = Date.parse(date);
   if (typeof t === 'number') {
     return new Date(Date.parse(date.replace(/-/g, '/')));
-  } else {
-    return new Date();
   }
+  return new Date();
 };
 
-export function parseWeekday(sDate: any) {
-  var dt = typeof sDate === 'string' ? new Date(sDate.replace(/-/g, '/')) : sDate;
-  var a = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+export function parseWeekday(sDate: string | Date) {
+  const dt = typeof sDate === 'string' ? new Date(sDate.replace(/-/g, '/')) : sDate;
+  const a = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
   return a[dt.getDay()];
 }
